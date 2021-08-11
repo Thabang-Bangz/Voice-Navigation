280adf76463e48f1891090d56bbaf9790c33d256
Thabang <thabangmahlangu321@gmail.com>
2021-08-11 13:15:17 +0200

    Add project files.

---
+++ Voice Navigation/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Voice_Navigation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnenable = new System.Windows.Forms.Button();
+            this.btndisable = new System.Windows.Forms.Button();
+            this.rtb11 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnenable
+            // 
+            this.btnenable.Location = new System.Drawing.Point(28, 221);
+            this.btnenable.Name = "btnenable";
+            this.btnenable.Size = new System.Drawing.Size(111, 52);
+            this.btnenable.TabIndex = 0;
+            this.btnenable.Text = "Enable Voice Control";
+            this.btnenable.UseVisualStyleBackColor = true;
+            this.btnenable.Click += new System.EventHandler(this.btnenable_Click);
+            // 
+            // btndisable
+            // 
+            this.btndisable.Enabled = false;
+            this.btndisable.Location = new System.Drawing.Point(242, 221);
+            this.btndisable.Name = "btndisable";
+            this.btndisable.Size = new System.Drawing.Size(103, 52);
+            this.btndisable.TabIndex = 1;
+            this.btndisable.Text = "Disable Voice Control";
+            this.btndisable.UseVisualStyleBackColor = true;
+            this.btndisable.Click += new System.EventHandler(this.btndisable_Click);
+            // 
+            // rtb11
+            // 
+            this.rtb11.Location = new System.Drawing.Point(28, 6);
+            this.rtb11.Name = "rtb11";
+            this.rtb11.Size = new System.Drawing.Size(317, 188);
+            this.rtb11.TabIndex = 2;
+            this.rtb11.Text = "-Ready To Go-";
+            this.rtb11.TextChanged += new System.EventHandler(this.rtb11_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 305);
+            this.Controls.Add(this.rtb11);
+            this.Controls.Add(this.btndisable);
+            this.Controls.Add(this.btnenable);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Voice Navigation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnenable;
+        private System.Windows.Forms.Button btndisable;
+        private System.Windows.Forms.RichTextBox rtb11;
+    }
+}
+
---
+++ Voice Navigation/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+
+
+namespace Voice_Navigation
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine spEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer sssizer = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenable_Click(object sender, EventArgs e)
+        {
+            spEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btndisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "say hello", "print my name" , "Speak selected text"});
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(commands);
+            Grammar grr = new Grammar(gb);
+
+            spEngine.LoadGrammarAsync(grr);
+            spEngine.SetInputToDefaultAudioDevice();
+            spEngine.SpeechRecognized += spEngine_SpeechRecognized;
+        }
+         
+        void spEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "say hello":
+                    PromptBuilder bb = new PromptBuilder();
+                    bb.StartSentence();
+                    bb.AppendText("Hello New User.");
+                    bb.EndSentence();
+
+                    bb.AppendBreak(new  TimeSpan());
+
+                    bb.StartSentence();
+                    bb.AppendText("How are you?", PromptEmphasis.Strong);
+                    bb.EndSentence();
+
+                    sssizer.SpeakAsync(bb);
+                    break;
+                case "print my name":
+                    rtb11.Text += "\nAshley";
+                    break;
+                case "speak selected text":
+                    sssizer.SpeakAsync(rtb11.SelectedText);
+                    break;
+            }
+        }
+
+        private void btndisable_Click(object sender, EventArgs e)
+        {
+            spEngine.RecognizeAsyncStop();
+            btndisable.Enabled = false;
+        }
+
+        private void rtb11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
